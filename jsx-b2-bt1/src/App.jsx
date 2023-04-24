75c41aac775950a260a09bb282b05a287e19a79c
ducanhle31 <leeducanh31@gmail.com>
2023-04-24 18:49:05 +0700

    bt2-jsx

---
+++ jsx-b2-bt1/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import React from "react";
+
+import FetchAPI from "./ProductsApi";
+
+function App() {
+  return (
+    <div className="App">
+      <FetchAPI />
+    </div>
+  );
+}
+
+export default App;
